bd83987cc6c0a02cd27dc115a224fbaf3356e48f
Matthew Molloy <whamtet@gmail.com>
2024-02-04 05:50:13 +0900

    sql

--- src/clj/simpleui/launchpoint/core.clj
+++ src/clj/simpleui/launchpoint/core.clj
@@ -11,7 +11,9 @@
 
     ;; Routes
     [simpleui.launchpoint.web.routes.api]
-    )
+    
+    [kit.edge.db.sql.conman] 
+    [kit.edge.db.sql.migratus])
   (:gen-class))
 
 ;; log uncaught exceptions in threads
