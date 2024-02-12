7f674859254ddaa34cead4176de4469826e28f38
Matthew Molloy <whamtet@gmail.com>
2024-02-12 20:54:34 +0900

    listen on 3001 instead

--- src/clj/simpleui/launchpoint/web/controllers/pdf.clj
+++ src/clj/simpleui/launchpoint/web/controllers/pdf.clj
@@ -1,7 +1,8 @@
 (ns simpleui.launchpoint.web.controllers.pdf
     (:require
         [clj-http.client :as client]
-        [clojure.data.json :as json]))
+        [clojure.data.json :as json]
+        [simpleui.launchpoint.env :refer [prod?]]))
 
 (defn- ->multipart [m]
     (mapv
@@ -9,11 +10,13 @@
          {:name (name k) :content v})
      m))
 
+;; docker run --rm --net=host -d gotenberg/gotenberg:8
 (defn pdf-profile [cookie]
     (:body
-        (client/post "http://localhost:3001/forms/chromium/convert/url"
+        (client/post "http://localhost:3000/forms/chromium/convert/url"
                      {:multipart
                       (->multipart
-                       {:url (format "http://%s:3000/api/profile" (System/getenv "DOCKER_HOST"))
+                       {:url (format "http://%s:3001/api/profile"
+                                     (if prod? "localhost" "host.docker.internal"))
                         :extraHttpHeaders (json/write-str {:cookie cookie})})
                       :as :stream})))
