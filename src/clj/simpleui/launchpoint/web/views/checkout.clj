ee0c1f87712095776fcc9049a255bffe1ce4445a
Matthew Molloy <whamtet@gmail.com>
2024-03-08 09:41:50 +0800

    subtotals, totals

--- src/clj/simpleui/launchpoint/web/views/checkout.clj
+++ src/clj/simpleui/launchpoint/web/views/checkout.clj
@@ -17,7 +17,10 @@
              :class "w-8 bg-clj-blue py-1.5 px-3 rounded-lg text-white"}
     label]])
 
-(defn- checkout-row [{:keys [id title image quantity]}]
+(defn- format$ [x]
+  (format "%.2f" x))
+
+(defn- checkout-row [{:keys [id title image price quantity]}]
   [:tr
    [:td
     [:img {:class "w-24"
@@ -27,43 +30,45 @@
    [:td
     [:div.flex.items-center
      [:div {:hx-post "checkout:inc"
-            :hx-vals {:inventory_id id}
-            :hx-target "#quantity"
-            :hx-swap "innerHTML"}
+            :hx-vals {:inventory_id id}}
       (button "+")]
-     [:div#quantity.mx-3 quantity]
+     [:div.mx-3 quantity]
      [:div {:hx-post "checkout:dec"
-            :hx-vals {:inventory_id id}
-            :hx-target "#quantity"
-            :hx-swap "innerHTML"}
-      (button "-")]]]])
+            :hx-vals {:inventory_id id}}
+      (button "-")]]]
+   [:td
+    [:div.mx-2 (format$ (* price quantity))]]])
+
+(defn- subtotal [orders]
+  (->> orders
+       (map #(* (:price %) (:quantity %)))
+       (apply +)))
 
 (defcomponent ^:endpoint checkout [req command ^:long inventory_id]
-  (case command
-        "inc"
-        (iam/do-auth
-         (item-order/inc-order req inventory_id))
-        "dec"
-        (iam/do-auth
-         (let [c (item-order/dec-order req inventory_id)]
-           (if (pos? c)
-             c
-             (do
-               (item-order/del-order req inventory_id)
-               response/hx-refresh))))
-        (let [{:keys [first_name]} (user/get-user req)
-              basket-count (item-order/basket-count req)]
-          [:div.min-h-screen.p-1 {:_ "on click add .hidden to .drop"}
-           [:a.absolute.top-3.left-3 {:href "/"}
-            [:img.w-24 {:src "/logo.svg"}]]
-           (dashboard/main-dropdown basket-count first_name)
-           [:div {:class "w-2/3 mx-auto text-center"}
-            [:div.my-5 (components/h1 (i18n "Checkout"))]
-            (if-let [orders (item-order/my-order req)]
-              [:table
-               [:tbody
-                (map checkout-row orders)]]
-              (i18n "Checkout is empty"))]])))
+  (iam/do-auth
+   (case command
+         "inc" (item-order/inc-order req inventory_id)
+         "dec"
+         (when-not (pos? (item-order/dec-order req inventory_id))
+                   (item-order/del-order req inventory_id))
+         nil))
+  (let [{:keys [first_name]} (user/get-user req)
+        basket-count (item-order/basket-count req)]
+    [:div.min-h-screen.p-1 {:_ "on click add .hidden to .drop"
+                            :hx-target "this"}
+     [:a.absolute.top-3.left-3 {:href "/"}
+      [:img.w-24 {:src "/logo.svg"}]]
+     (dashboard/main-dropdown basket-count first_name)
+     [:div {:class "w-2/3 mx-auto text-center"}
+      [:div.my-5 (components/h1 (i18n "Checkout"))]
+      (if-let [orders (item-order/my-order req)]
+        [:div
+         [:table
+          [:tbody
+           (map checkout-row orders)]]
+         [:hr.my-2.border]
+         (components/h2 (i18n "Total") " " (format$ (subtotal orders)))]
+        (i18n "Checkout is empty"))]]))
 
 (defn ui-routes [{:keys [query-fn]}]
   (simpleui/make-routes
