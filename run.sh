a24e5b3cec3b4dc7b8ee3693fd4e527f1458bdb0
Matthew Molloy <whamtet@gmail.com>
2025-05-29 06:10:59 +0900

    update repl port

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -Dclojure.server.repl="{:port 5556 :accept clojure.core.server/repl}" -jar target/launchpoint-standalone.jar
+java -Dclojure.server.repl="{:port 5551 :accept clojure.core.server/repl}" -jar target/launchpoint-standalone.jar
