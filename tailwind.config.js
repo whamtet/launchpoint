41180759ca225eaa1662c57e8d38e1aff2730ccb
Matthew Molloy <whamtet@gmail.com>
2025-06-19 03:28:59 +0900

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ["./tailwind/**/*.{html,js}"],
+  content: ["./src/**/*.clj"],
   theme: {
     extend: {},
     colors: {
